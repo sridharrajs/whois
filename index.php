d2d7b22dcba272b56a332fc8d1d1ae3d1d03f346
sridharrajs <me@sridhar.co>
2015-12-16 17:51:35 +0530

    sample commit

--- index.php
+++ index.php
@@ -4,23 +4,18 @@
 Speedy Whois - A website for whois queries, based on the phpWhois class.
 Copyleft by Uri Even-Chen, Speedy Software.
 http://www.speedywhois.com/
-
 This program is free software; you can redistribute it and/or
 modify it under the terms of the GNU General Public License
 as published by the Free Software Foundation; either version 2
 of the License, or (at your option) any later version.
-
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 */
-
 $query= trim(@$_POST['query']);
-
 header('Content-Type: text/html; charset=UTF-8');
-
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
