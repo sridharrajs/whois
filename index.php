60d1e4222855c96381092abb33937fa02d97929b
sridharrajs <me@sridhar.co>
2016-01-05 09:13:20 +0530

    adding Piwik analytics code

--- index.php
+++ index.php
@@ -72,5 +72,26 @@ header('Content-Type: text/html; charset=UTF-8');
 <script type="text/javascript" src="js/bootstrap.min.js"></script>
 <script type="text/javascript" src="js/scripts.js"></script>
 
+<!-- Piwik -->
+<script type="text/javascript">
+    var _paq = _paq || [];
+    _paq.push(['trackPageView']);
+    _paq.push(['enableLinkTracking']);
+    (function () {
+        var u = "//sridhar.co/piwik/";
+        _paq.push(['setTrackerUrl', u + 'piwik.php']);
+        _paq.push(['setSiteId', 4]);
+        var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
+        g.type = 'text/javascript';
+        g.async = true;
+        g.defer = true;
+        g.src = u + 'piwik.js';
+        s.parentNode.insertBefore(g, s);
+    })();
+</script>
+<noscript><p><img src="//sridhar.co/piwik/piwik.php?idsite=4" style="border:0;" alt=""/></p></noscript>
+<!-- End Piwik Code -->
+
+
 </body>
 </html>
