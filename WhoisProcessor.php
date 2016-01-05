5e16d6bbfef0ce4398d5a1121a7b3548b052153c
sridharrajs <me@sridhar.co>
2016-01-05 08:41:31 +0530

    #1 fixed

--- WhoisProcessor.php
+++ WhoisProcessor.php
@@ -3,7 +3,8 @@ $query = $_GET['query'];
 include_once('phpwhois/whois.main.php');
 include_once('phpwhoisutils/whois.utils.php');
 $whois = new Whois();
-$result = $whois->Lookup($query);
+$naked_domain = getNakedDomain($query);
+$result = $whois->Lookup($naked_domain);
 if (!empty($result['rawdata'])) {
     $utils = new utils;
     echo $utils->showHTML($result);
@@ -11,3 +12,14 @@ if (!empty($result['rawdata'])) {
     echo implode($whois->Query['errstr'], "<br />\n");
 }
 echo "<br />\n";
+
+
+function getNakedDomain($input)
+{
+    $input = trim($input, '/');
+    if (!preg_match('#^http(s)?://#', $input)) {
+        $input = 'http://' . $input;
+    }
+    $urlParts = parse_url($input);
+    return preg_replace('/^www\./', '', $urlParts['host']);
+}
